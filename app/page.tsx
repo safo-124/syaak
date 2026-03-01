7b46e7a50856116f9196390dce7f36f5e4410433
safo-124 <nanasafo122@gmail.com>
2026-03-01 09:37:57 +0200

    Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
 import HomePage from "./(marketing)/page"
+import { SiteHeader } from "@/components/layout/site-header"
+import { SiteFooter } from "@/components/layout/site-footer"
 
 export default function RootPage() {
-  return <HomePage />
+  return (
+    <div className="flex min-h-screen flex-col bg-background text-foreground">
+      <SiteHeader />
+      <main className="flex-1">
+        <HomePage />
+      </main>
+      <SiteFooter />
+    </div>
+  )
 }
